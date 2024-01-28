ecd6f002e8c83c1c7137231a8fd231e3c2e3638d
gio54321 <g.dellimmagine@gmail.com>
2024-01-29 00:40:58 +0100

    Add support for searching books

--- src/html/header.php
+++ src/html/header.php
@@ -49,7 +49,10 @@ session_start();
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
             </svg>
           </div>
-          <input type="text" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search...">
+          <form action="/search.php" method="get">
+            <input type="text" id="search-navbar" name="q" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search...">
+            <input type="submit" hidden />
+          </form>
         </div>
         <button data-collapse-toggle="navbar-search" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-search" aria-expanded="false">
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -66,7 +69,10 @@ session_start();
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
             </svg>
           </div>
-          <input type="text" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search...">
+          <form action="/search.php" method="get">
+            <input type="text" id="search-navbar" name="q" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search...">
+            <input type="submit" hidden />
+          </form>
         </div>
         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
           <li>
---
+++ src/search.php
@@ -0,0 +1,46 @@
+<?php
+require_once __DIR__ . '/utils.php';
+
+if (!isset($_GET['q']) || !is_string($_GET['q'])) {
+    header('Location: /');
+    exit;
+}
+
+
+$books = execute_query('SELECT * FROM books WHERE LOWER(title) LIKE CONCAT("%", :q, "%")', ['q' => $_GET['q']])->fetchAll();
+require_once __DIR__ . '/html/header.php';
+?>
+
+<div class="flex items-center justify-center mt-10 mb-10 text-xl font-semibold">
+    Search results for "<?php echo htmlspecialchars($_GET['q']) ?>" (<?php echo count($books) ?> items)
+</div>
+<div class="flex items-center justify-center mt-10 mb-10">
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <?php foreach ($books as $book) { ?>
+            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                <div class="grid place-items-center">
+                    <a href="/book.php?id=<?php echo $book['id'] ?>">
+                        <img class="p-8 rounded-t-lg h-96" src="<?php echo $book["image"] ?>" alt="product image" />
+                    </a>
+                </div>
+                <div class="px-5 pb-5">
+                    <a href="/book.php?id=<?php echo $book['id'] ?>">
+                        <h5 class="text-xl font-semibold tracking-tight text-gray-900">
+                            <?php echo $book['title'] ?>
+                        </h5>
+                    </a>
+                    <div class="flex items-center justify-between mt-4">
+                        <span class="text-3xl text-gray-900">
+                            <?php echo $book['price'] / 100 ?> €
+                        </span>
+                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</div>
+
+<?php
+require_once __DIR__ . '/html/footer.php';
+?>
