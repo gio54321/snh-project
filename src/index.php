8cbe43e418d1545be44beb7f31984b3e1fd43382
gio54321 <g.dellimmagine@gmail.com>
2024-01-28 16:11:48 +0100

    More dark removal

--- src/index.php
+++ src/index.php
@@ -23,10 +23,10 @@ $books = $stmt->fetchAll();
                         </h5>
                     </a>
                     <div class="flex items-center justify-between">
-                        <span class="text-3xl font-bold text-gray-900">
-                            $<?php echo $book['price'] / 100 ?>
+                        <span class="text-3xl text-gray-900">
+                            <?php echo $book['price'] / 100 ?> €
                         </span>
-                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</a>
                     </div>
                 </div>
             </div>
