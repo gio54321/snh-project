e04a88c84cdb33abb7c39992acc8f91cddf60974
gio54321 <g.dellimmagine@gmail.com>
2024-01-31 23:27:22 +0100

    Remove session_destroy

--- src/logout.php
+++ src/logout.php
@@ -2,11 +2,9 @@
 
 require_once __DIR__ . '/utils.php';
 
-//TODO check CSRF token
 unset($_SESSION['user_id']);
 unset($_SESSION['username']);
 unset($_SESSION['csrf_token']);
-session_destroy();
 
 header('Location: /');
 exit;
